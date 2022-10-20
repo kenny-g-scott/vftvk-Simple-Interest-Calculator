ca58705db3cc9f5b9507b8707a2a9a9405da6ae8
Kenny Scott <kenny@bluefireinteractive.com>
2022-10-19 21:38:04 -0700

    initial changes

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() {
+    var principal = document.getElementById("principal").value;
     
-}
-        
+    if (principal <= 0) {
+      alert("Enter a positive number");
+      document.getElementById("principal").focus();
+      return false;
+    }
+  
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = (principal * years * rate) / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var result = document.getElementById("result");
+  
+    result.innerHTML = `If you deposit <strong>${principal}</strong>,<br>
+      at an interest rate of <strong>${rate}%</strong>.<br>
+      You will receive an amount of <strong>${interest}</strong>,<br>
+      in the year <strong>${year}</strong>`;
+  }
+  
+  function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+  }
+  
