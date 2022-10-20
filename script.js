3b4547ce893382c8e2d37fef450294a4b8c2e09f
Kenny Scott <kenny@bluefireinteractive.com>
2022-10-19 21:59:32 -0700

    final changes to reflect project spec

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ function compute() {
     var year = new Date().getFullYear() + parseInt(years);
     var result = document.getElementById("result");
   
-    result.innerHTML = `If you deposit <strong>${principal}</strong>,<br>
-      at an interest rate of <strong>${rate}%</strong>.<br>
-      You will receive an amount of <strong>${interest}</strong>,<br>
-      in the year <strong>${year}</strong>`;
+    result.innerHTML = `If you deposit <mark>${principal}</mark>,<br>
+      at an interest rate of <mark>${rate}%</mark>.<br>
+      You will receive an amount of <mark>${interest}</mark>,<br>
+      in the year <mark>${year}</mark>`;
   }
   
   function updateRate() {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
+    document.getElementById("rate_val").innerText = rateval + "%";
   }
   
